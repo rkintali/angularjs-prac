a7eb90f382a86f31082c83b3dcb531486033226d
RK <rajsekhar.kintali@gmail.com>
2016-04-16 08:18:17 +1000

    Initial commit

---
+++ angularjs-prac/src/main/java/com/rk/prac/App.java
@@ -0,0 +1,7 @@
+package com.rk.prac;
+
+public class App {
+	public static void main(String... args){
+		System.out.println("This is the rythm of the life");
+	}
+}
---
+++ angularjs-prac/src/test/java/com/rk/prac/AppTest.java
@@ -0,0 +1,7 @@
+package com.rk.prac;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+}
