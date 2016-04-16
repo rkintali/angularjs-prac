873d009c75e501e07f601e4aaedff6667d238d8f
RK <rajsekhar.kintali@gmail.com>
2016-04-16 18:43:06 +1000

    ng-models, forms practice

--- angularjs-prac/src/main/webapp/js/app.js
+++ angularjs-prac/src/main/webapp/js/app.js
@@ -12,21 +12,40 @@
 		description : 'Game of thrones Gem',
 		canPurchase : true,
 		soldOut : false,
+		reviews : [ {
+			by : "RK",
+			rating : "5",
+			comments : "This is a comment",
+		} ],
 	}, {
 		name : 'Drogo',
 		price : 100,
 		description : 'Another gem from Game of thrones',
 		canPurchase : true,
 		soldOut : false,
-	} ]
+		reviews : [ {
+			by : "RK",
+			rating : "5",
+			comments : "This is a comment",
+		} ],
+	} ];
 	app.controller("panelController", function() {
-		this.tab = 1;
-		this.selectTab = function(selectTab){
-			this.tab=selectTab;
+		this.tab = 3;
+		this.selectTab = function(selectTab) {
+			this.tab = selectTab;
 		};
-		this.isSelected = function(selectedTab){
+		this.isSelected = function(selectedTab) {
 			return this.tab == selectedTab;
 		};
-		
-	})
+
+	});
+	
+	app.controller('reviewController', function(){
+		this.review = {};
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review={};
+		}
+	});
+	
 })();
